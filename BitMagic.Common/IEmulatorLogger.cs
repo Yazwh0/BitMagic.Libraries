f04883296a42c5cf2a517b88112fbd7a15ec0974
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2023-03-31 11:03:49 +0100

    Create IEmulatorLogger.cs
    Adds logger

---
+++ BitMagic.Common/IEmulatorLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitMagic.Common
+{
+    public interface IEmulatorLogger
+    {
+        public void Log(string message);
+        public void LogLine(string message);
+        public void LogError(string message);
+    }
+}

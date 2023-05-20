21017e09323d0966f10112504be6621dc8813a47
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2023-05-20 14:58:41 +0100

    Create ISourceFile.cs

---
+++ BitMagic.Common/ISourceFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BitMagic.Common;
+
+public interface ISourceFile
+{
+    string Name { get; set; }
+    string Path { get; set; }
+    int ReferenceId { get; set; }
+    string Origin { get; set; }
+    bool Volatile { get; set; }
+    Action Generate { get; set; }
+
+    string GetContent();
+}

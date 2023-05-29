972bfc990246654323b44c74d08e5215caf054a7
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2023-05-29 17:00:55 +0100

    Use namespace

--- BitMagic.Common/IDisplay.cs
+++ BitMagic.Common/IDisplay.cs
@@ -1,76 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BitMagic.Common
+namespace BitMagic.Common;
+
+public class BitImage
 {
-    public class BitImage
+    public Memory<PixelRgba> DrawPixels { get; internal set; }
+    public Memory<PixelRgba> RenderPixels { get; internal set; }
+    
+    private PixelRgba[] _pixelsA;
+    private PixelRgba[] _pixelsB;
+
+    private bool _state = false;
+
+    public int Width { get; }
+    public int Height { get; }
+
+    public BitImage(int width, int height)
     {
-        public Memory<PixelRgba> DrawPixels { get; internal set; }
-        public Memory<PixelRgba> RenderPixels { get; internal set; }
-        
-        private PixelRgba[] _pixelsA;
-        private PixelRgba[] _pixelsB;
+        _pixelsA = new PixelRgba[height * width];
+        _pixelsB = new PixelRgba[height * width];
+        DrawPixels = _pixelsA;
+        RenderPixels = _pixelsB;
+        Width = width;
+        Height = height;
+    }
 
-        private bool _state = false;
+    public void Switch()
+    {
+        _state = !_state;
 
-        public int Width { get; }
-        public int Height { get; }
-
-        public BitImage(int width, int height)
+        if (_state)
         {
-            _pixelsA = new PixelRgba[height * width];
-            _pixelsB = new PixelRgba[height * width];
             DrawPixels = _pixelsA;
             RenderPixels = _pixelsB;
-            Width = width;
-            Height = height;
-        }
-
-        public void Switch()
+        } 
+        else
         {
-            _state = !_state;
-
-            if (_state)
-            {
-                DrawPixels = _pixelsA;
-                RenderPixels = _pixelsB;
-            } 
-            else
-            {
-                DrawPixels = _pixelsB;
-                RenderPixels = _pixelsA;
-            }
+            DrawPixels = _pixelsB;
+            RenderPixels = _pixelsA;
         }
     }
+}
 
-    [StructLayout(LayoutKind.Sequential)]
-    public struct PixelRgba
+[StructLayout(LayoutKind.Sequential)]
+public struct PixelRgba
+{
+    public byte R;
+    public byte G;
+    public byte B;
+    public byte A;
+
+    public PixelRgba()
     {
-        public byte R;
-        public byte G;
-        public byte B;
-        public byte A;
-
-        public PixelRgba()
-        {
-            R = 0;
-            G = 0;
-            B = 0;
-            A = 0;
-        }
-
-        public PixelRgba(byte r, byte g, byte b, byte a = 0xff)
-        {
-            R = r;
-            G = g;
-            B = b;
-            A = a;
-        }
-
-        public override string ToString() => $"R:${R:X2} G:${G:X2} B:${B:X2} A:${A:X2}";
+        R = 0;
+        G = 0;
+        B = 0;
+        A = 0;
     }
+
+    public PixelRgba(byte r, byte g, byte b, byte a = 0xff)
+    {
+        R = r;
+        G = g;
+        B = b;
+        A = a;
+    }
+
+    public override string ToString() => $"R:${R:X2} G:${G:X2} B:${B:X2} A:${A:X2}";
 }

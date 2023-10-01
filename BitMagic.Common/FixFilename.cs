b16bd2cf4e981378a94bc6555e800d15f5a6966e
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2023-10-01 12:31:44 +0100

    Add FixFilename to common

---
+++ BitMagic.Common/FixFilename.cs
@@ -0,0 +1,14 @@
+﻿namespace BitMagic.Common;
+
+public static class FixFilenameExtension
+{
+    public static string FixFilename(this string filename)
+    {
+#if OS_WINDOWS
+        return char.ToLower(filename[0]) + filename[1..];
+#endif
+#if OS_LINUX
+        return path;
+#endif
+    }
+}

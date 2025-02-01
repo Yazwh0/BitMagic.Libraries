84a5a5c87c70d1250ca9a6c6b00a84fa314cac22
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2025-02-01 12:37:36 +0000

    Fix filename is now case aware for windows

--- BitMagic.Common/FixFilename.cs
+++ BitMagic.Common/FixFilename.cs
@@ -1,11 +1,37 @@
-﻿namespace BitMagic.Common;
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace BitMagic.Common;
 
 public static class FixFilenameExtension
 {
     public static string FixFilename(this string filename)
     {
 #if OS_WINDOWS
-        return char.ToLower(filename[0]) + filename[1..];
+        string fullFilePath = Path.GetFullPath(filename);
+
+        string fixedPath = "";
+        foreach (string token in fullFilePath.Split('\\'))
+        {
+            //first token should be drive token
+            if (fixedPath == "")
+            {
+                //fix drive casing
+                string drive = string.Concat(token, "\\");
+                drive = DriveInfo.GetDrives()
+                    .First(driveInfo => driveInfo.Name.Equals(drive, StringComparison.OrdinalIgnoreCase)).Name;
+
+                fixedPath = drive;
+            }
+            else
+            {
+                fixedPath = Directory.GetFileSystemEntries(fixedPath, token).First();
+            }
+        }
+
+        return fixedPath;
+        //return char.ToLower(filename[0]) + filename[1..];
 #endif
 #if OS_LINUX
         return filename;

376e6cf8d3dd6962c15454a2d3b9921e4b3ab3cc
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2025-09-27 10:14:44 +0100

    Add debug breakpoint constant

--- BitMagic.Common/DebugConstants.cs
+++ BitMagic.Common/DebugConstants.cs
@@ -2,8 +2,11 @@
 
 public static class DebugConstants
 {
-    public static readonly uint Breakpoint = 0x01;
-    public static readonly uint SystemBreakpoint = 0x02;
-    public static readonly uint NoStop = 0x04;
-    public static readonly uint Exception = 0x08;
+    public static readonly uint Breakpoint = 0b0000_0001;
+    public static readonly uint SystemBreakpoint = 0b0000_0010;
+    public static readonly uint NoStop = 0b0000_0100;
+    public static readonly uint Exception = 0b0000_1000;
+
+    // Specific breakpoints.
+    public static readonly uint LoadCheck = 0b0001_0010;
 }

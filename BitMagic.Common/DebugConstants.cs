e4d2b834b17c9697668c4d81dfdf8930df0ae272
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2026-02-09 21:01:27 +0000

    Add constants

--- BitMagic.Common/DebugConstants.cs
+++ BitMagic.Common/DebugConstants.cs
@@ -2,11 +2,13 @@
 
 public static class DebugConstants
 {
-    public static readonly uint Breakpoint = 0b0000_0001;
-    public static readonly uint SystemBreakpoint = 0b0000_0010;
-    public static readonly uint NoStop = 0b0000_0100;
-    public static readonly uint Exception = 0b0000_1000;
+    public static readonly uint Breakpoint          = 0b0000_0001;
+    public static readonly uint SystemBreakpoint    = 0b0000_0010;
+    public static readonly uint NoStop              = 0b0000_0100;
+    public static readonly uint Exception           = 0b0000_1000;
+    public static readonly uint ReadWriteFlags      = 0b1111_0000;
 
     // Specific breakpoints.
-    public static readonly uint LoadCheck = 0b0001_0010;
+    public static readonly uint LoadCheck           = 0b0001_0010;  // can use bits in the RW mask because its on the stack
+    public static readonly uint DebugAction         = 0b0000_1010;  // the other 3 bytes are used to locate the action. one per memory location limitation.
 }
